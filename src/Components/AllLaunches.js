d9b9cf32c8ca84b1607261e65d1e863aa048ccd5
unknown <webcode87@gmail.com>
2021-02-23 01:47:16 +0530

    file structure change

--- src/Components/AllLaunches.js
+++ src/Components/AllLaunches.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
 
 import './AllLaunches.css';
+import Header from './Header';
+import DataList from './DataList';
 import Pagination from './Pagination';
 
 const AllLaunches = (props) => {
   const [data, setData] = useState([]);
   const [loading, SetLoading] = useState(true);
   const [currentPage, SetCurrentPage] = useState(1);
+  const [listPerPage] = useState(12);
 
   const url = 'https://api.spacexdata.com/v3/launches';
 
@@ -22,59 +25,27 @@ const AllLaunches = (props) => {
 
   // get current post
 
-  const firstPost = 1 * 12;
-  const lastPost = firstPost - 12;
-  const currentPost = data.slice(1, 12);
-  console.log(
-    firstPost,
-    lastPost,
-    currentPost,
-    'length is ' + Math.floor(data.length / 12)
-  );
+  const lastPage = currentPage * listPerPage;
+  const firstPage = lastPage - listPerPage;
+  const activePage = data.slice(firstPage, lastPage);
+
+  const pagination = (pageNumber) => SetCurrentPage(pageNumber);
 
   return (
-    <div className="dataContainer">
-      <>
-        <table>
-          <thead>
-            <tr>
-              <th>No:</th>
-              <th>Launches (UTC):</th>
-              <th>Location</th>
-              <th>Mission</th>
-              <th>Orbit</th>
-              <th>Launches Status</th>
-              <th>Rocket</th>
-            </tr>
-          </thead>
-
-          {!loading ? (
-            <tbody>
-              {currentPost.map((post, key) => (
-                <tr key={key}>
-                  <td>{post.flight_number}</td>
-                  <td>{post.launch_date_utc}</td>
-                  <td>{post.launch_site.site_name}</td>
-                  <td>{post.mission_name}</td>
-                  <td>{post.rocket.second_stage.payloads[0].orbit}</td>
-                  <td>{post.launch_success ? 'Success' : 'Failed'}</td>
-                  <td>{post.rocket.rocket_name}</td>
-                </tr>
-              ))}
-            </tbody>
-          ) : (
-            <tbody>
-              <tr>
-                <td colSpan={'7'} className="loadingBox">
-                  <img src="https://media3.giphy.com/media/3oEjI6SIIHBdRxXI40/200.gif" />
-                </td>
-              </tr>
-            </tbody>
-          )}
-        </table>
-      </>
-      <Pagination />
-    </div>
+    <>
+      <Header />
+      <div className="mainContainer">
+        <div className="dataContainer">
+          <DataList loading={loading} data={activePage} />
+        </div>
+        <Pagination
+          listPerPage={listPerPage}
+          pagination={pagination}
+          currentPage={currentPage}
+          totalPage={data.length}
+        />
+      </div>
+    </>
   );
 };
 
