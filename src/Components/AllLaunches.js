e8a7c690671ed748395b743673753792aa391995
shajad1234 <webcode87@gmail.com>
2021-02-22 19:09:00 +0530

    pagination creating

--- src/Components/AllLaunches.js
+++ src/Components/AllLaunches.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
 
 import './AllLaunches.css';
+import Pagination from './Pagination';
 
 const AllLaunches = (props) => {
   const [data, setData] = useState([]);
   const [loading, SetLoading] = useState(true);
+  const [currentPage, SetCurrentPage] = useState(1);
 
-  const url = 'https://api.spacexdata.com/v3/launches/upcoming';
+  const url = 'https://api.spacexdata.com/v3/launches';
 
   useEffect(() => {
     async function loadData() {
@@ -18,37 +20,60 @@ const AllLaunches = (props) => {
     loadData();
   }, []);
 
-  return (
-    <div style={{ height: '450px', boxShadow: '0 2px 5px 5px solid grey' }}>
-      <table>
-        <tr>
-          <th>No:</th>
-          <th>Launches (UTC):</th>
-          <th>Location</th>
-          <th>Mission</th>
-          <th>Orbit</th>
-          <th>Launches Status</th>
-          <th>Rocket</th>
-        </tr>
+  // get current post
 
-        {!loading ? (
-          <>
-            {data.map((val, key) => (
-              <tr key={key}>
-                <td>{val.flight_number}</td>
-                <td>{val.launch_date_utc}</td>
-                <td>{val.launch_site.site_name}</td>
-                <td>{val.mission_name}</td>
-                <td>{val.rocket.second_stage.payloads[0].orbit}</td>
-                <td>{val.launch_success ? 'Success' : 'Failed'}</td>
-                <td>{val.rocket.rocket_name}</td>
+  const firstPost = 1 * 12;
+  const lastPost = firstPost - 12;
+  const currentPost = data.slice(1, 12);
+  console.log(
+    firstPost,
+    lastPost,
+    currentPost,
+    'length is ' + Math.floor(data.length / 12)
+  );
+
+  return (
+    <div className="dataContainer">
+      <>
+        <table>
+          <thead>
+            <tr>
+              <th>No:</th>
+              <th>Launches (UTC):</th>
+              <th>Location</th>
+              <th>Mission</th>
+              <th>Orbit</th>
+              <th>Launches Status</th>
+              <th>Rocket</th>
+            </tr>
+          </thead>
+
+          {!loading ? (
+            <tbody>
+              {currentPost.map((post, key) => (
+                <tr key={key}>
+                  <td>{post.flight_number}</td>
+                  <td>{post.launch_date_utc}</td>
+                  <td>{post.launch_site.site_name}</td>
+                  <td>{post.mission_name}</td>
+                  <td>{post.rocket.second_stage.payloads[0].orbit}</td>
+                  <td>{post.launch_success ? 'Success' : 'Failed'}</td>
+                  <td>{post.rocket.rocket_name}</td>
+                </tr>
+              ))}
+            </tbody>
+          ) : (
+            <tbody>
+              <tr>
+                <td colSpan={'7'} className="loadingBox">
+                  <img src="https://media3.giphy.com/media/3oEjI6SIIHBdRxXI40/200.gif" />
+                </td>
               </tr>
-            ))}
-          </>
-        ) : (
-          <div>loading...</div>
-        )}
-      </table>
+            </tbody>
+          )}
+        </table>
+      </>
+      <Pagination />
     </div>
   );
 };
---
+++ src/Components/Pagination/Pagination.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Pagination.css';
+
+const Pagination = (props) => {
+  return (
+    <div className="pagination">
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Pagination;
