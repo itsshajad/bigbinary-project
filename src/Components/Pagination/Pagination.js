9b9beeb4715735ecc7f41853bf1a9e209db7bbfd
unknown <webcode87@gmail.com>
2021-02-23 01:45:03 +0530

    pagination created

--- src/Components/Pagination/Pagination.js
+++ src/Components/Pagination/Pagination.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import './Pagination.css';
 
-const Pagination = (props) => {
+const Pagination = ({ pagination, totalPage, listPerPage, currentPage }) => {
+  const pageNumber = [];
+
+  for (let i = 1; i <= Math.ceil(totalPage / listPerPage); i++) {
+    pageNumber.push(i);
+  }
+  const PageDecrease = () => {
+    if (currentPage > 1) {
+      pagination(currentPage - 1);
+    }
+  };
+  const PageIncrease = () => {
+    if (currentPage < pageNumber.length) {
+      pagination(currentPage + 1);
+    }
+  };
   return (
     <div className="pagination">
       <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-        <li>5</li>
+        <li onClick={PageDecrease}> {'<'} </li>
+        {pageNumber.map((pageNumber) => (
+          <li onClick={() => pagination(pageNumber)} key={pageNumber}>
+            {pageNumber}
+          </li>
+        ))}
+        <li onClick={PageIncrease}> {'>'} </li>
       </ul>
     </div>
   );
