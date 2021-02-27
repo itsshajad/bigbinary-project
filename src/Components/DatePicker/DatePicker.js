137deb7b73485a91701ace68048ab3a150083757
shajad1234 <webcode87@gmail.com>
2021-02-27 18:46:27 +0530

    date filter popup created

--- src/Components/DatePicker/DatePicker.js
+++ src/Components/DatePicker/DatePicker.js
@@ -1,33 +1,101 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import moment from 'moment';
 
-const DatePicker = () => {
+const DatePicker = ({ setEnd, setStart }) => {
   const [open, setOpen] = useState(false);
-  const [value, setValue] = useState('past 4 month');
+  const [startDate, setStartDate] = useState('2015-06-22');
+  const [endDate, setEndDate] = useState(moment().format('YYYY-MM-DD'));
 
   const handleOpen = () => {
-    setOpen(true);
+    setOpen(!open);
+    if (!open) {
+      document.querySelector('body').style.overflow = 'hidden';
+    } else {
+      document.querySelector('body').style.overflow = 'auto';
+    }
   };
 
-  // const urlValue = new URLSearchParams(window.location.search).get('launches');
-  const history = useHistory();
-  const param = new URLSearchParams();
+  useEffect(() => {
+    const urlValue = new URLSearchParams(window.location.search);
 
-  if (value) {
-    param.append('filter', value);
-  } else {
-    param.delete('filter');
-  }
-  history.push({ search: param.toString() });
+    console.log(urlValue.get('start'), urlValue.get('end'));
+
+    setEnd(urlValue.get('end'));
+    setStart(urlValue.get('start'));
+  }, [setEnd, setStart, startDate, endDate]);
 
   return (
     <div>
-      <h3 onClick={handleOpen}>{value}</h3>
+      <h3 onClick={handleOpen}>{startDate ? startDate : ' select'}</h3>
+
       {open && (
-        <div>
-          <p onClick={() => setValue('past 4 month')}>past 4 month</p>
-          <p onClick={() => setValue('past 6 month')}>past 6 month</p>
-          <p onClick={() => setValue('past 8 month')}>past 8 month</p>
+        <div className="popupContainer">
+          <div className="popupBody">
+            <span className={'close'} onClick={handleOpen}>
+              &#x2715;
+            </span>
+            <div className="datePickerBody">
+              <div className="leftColumn">
+                <ul>
+                  <li
+                    onClick={() =>
+                      setStart(moment().add(-1, 'week').format('YYYY-MM-DD'))
+                    }
+                  >
+                    Past Week
+                  </li>
+                  <li
+                    onClick={() =>
+                      setStart(moment().add(-1, 'month').format('YYYY-MM-DD'))
+                    }
+                  >
+                    Past Month
+                  </li>
+                  <li
+                    onClick={() =>
+                      setStart(moment().add(-3, 'month').format('YYYY-MM-DD'))
+                    }
+                  >
+                    Past 3 Month
+                  </li>
+                  <li
+                    onClick={() => {
+                      setStart(moment().add(-6, 'month').format('YYYY-MM-DD'));
+                    }}
+                  >
+                    Past 6 Month
+                  </li>
+                  <li
+                    onClick={() => {
+                      setStart(moment().add(-1, 'y').format('YYYY-MM-DD'));
+                    }}
+                  >
+                    Past year
+                  </li>
+                  <li
+                    onClick={() => {
+                      setStart(moment().add(-2, 'y').format('YYYY-MM-DD'));
+                    }}
+                  >
+                    Past 2 year
+                  </li>
+                </ul>
+              </div>
+
+              <div className="rightColumn">
+                <input
+                  type="date"
+                  name="start"
+                  onChange={(e) => setStart(e.target.value)}
+                />
+                <input
+                  type="date"
+                  name="end"
+                  onChange={(e) => setEnd(e.target.value)}
+                />
+              </div>
+            </div>
+          </div>
         </div>
       )}
     </div>
