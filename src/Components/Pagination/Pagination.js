28e499e259f02d8665ae9af895697ad7d9bf3dc1
unknown <webcode87@gmail.com>
2021-03-02 23:30:47 +0530

    pagination created

--- src/Components/Pagination/Pagination.js
+++ src/Components/Pagination/Pagination.js
@@ -6,18 +6,31 @@ const Pagination = ({ pagination, totalList, listPerPage, currentPage }) => {
     pageNumber.push(i);
   }
 
+  console.log(currentPage, totalPage);
   // page decrement
   const PageDecrease = () => {
     if (currentPage > 1) {
       pagination(currentPage - 1);
     }
   };
+  const PageDecreaseDouble = () => {
+    if (currentPage === totalPage) {
+      pagination(currentPage - 2);
+    }
+  };
+
   // page increment
   const PageIncrease = () => {
     if (currentPage < pageNumber.length) {
       pagination(currentPage + 1);
     }
   };
+  const PageIncreaseDouble = () => {
+    if (currentPage < totalPage - 1) {
+      pagination(currentPage + 2);
+    }
+  };
+
   return (
     <div className="pagination">
       {pageNumber.length > 4 && (
@@ -28,17 +41,39 @@ const Pagination = ({ pagination, totalList, listPerPage, currentPage }) => {
           >
             {'<'}
           </li>
-          <li onClick={() => pagination(currentPage + 1)}>{currentPage + 1}</li>
-          {currentPage === totalPage ? (
-            <li onClick={() => PageDecrease()}>...</li>
-          ) : null}
-          <li onClick={() => PageIncrease(currentPage + 2)}>
-            {currentPage + 2}
+
+          <li
+            onClick={() =>
+              pagination(
+                currentPage === totalPage
+                  ? currentPage - totalPage + 1
+                  : currentPage
+              )
+            }
+          >
+            {currentPage === totalPage
+              ? currentPage - totalPage + 1
+              : currentPage}
           </li>
-          {currentPage < totalPage ? (
-            <li onClick={() => PageIncrease()}>...</li>
+          {currentPage === totalPage ? (
+            <li onClick={() => PageDecreaseDouble()}>...</li>
           ) : null}
+          <li
+            onClick={() =>
+              pagination(
+                currentPage >= totalPage ? currentPage - 1 : currentPage + 1
+              )
+            }
+          >
+            {currentPage >= totalPage ? currentPage - 1 : currentPage + 1}
+          </li>
+
+          {currentPage < totalPage ? (
+            <li onClick={() => PageIncreaseDouble()}>...</li>
+          ) : null}
+
           <li onClick={() => pagination(totalPage)}>{totalPage}</li>
+
           <li
             className={currentPage >= totalPage ? 'disabledPagination' : ''}
             onClick={PageIncrease}
